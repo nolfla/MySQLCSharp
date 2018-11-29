52d0f2ae0d290d0213e7b57c5e3bda8ce00f9f20
Nolan Flatt <nolf@gmx.com>
2018-11-28 22:05:01 -0700

    V1 of MySQL C# Files

---
+++ V1/MySqlConn/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace MySqlConn
+{
+    public partial class Form1 : Form
+
+        MySqlConnection conn;
+        string connString;
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlBaseConnectionStringBuilder = "SERVER = ; PORT = ; DATABASE = ; UID = ; PASSWORD = ;";
+
+            try
+            {
+                ConnectionState = new MySqlConnection();
+                conn.ConnectionString = connString;
+                conn.Open();
+                MessageBox.Show("Successfully connected");
+            }
+
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
